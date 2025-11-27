7d3c7aa6e4accfe9ad348bc3662728ca239e4b3f
p3220152 <giopro109@gmail.com>
2025-11-27 15:27:55 +0200

    jekinhma corses

---
+++ part-a-frontend/assets/js/courses.js
@@ -0,0 +1,8 @@
+const courses=[
+    {
+        title: "Εισαγωγή στον Προγραμματισμό",
+        description: "Μάθε τα βασικά του προγραμματισμού με γλώσσες όπως Python και JavaScript.",
+        level: "begginer",
+        link: "courses/intro-to-programming.html"
+    }
+]
